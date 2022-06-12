82ee7bdb3bf5b36f2513f7bf3b5f869d53e14a18
rueyhow <rahow32@gmail.com>
2022-06-13 00:31:13 +0800

    idk

---
+++ ASN2_Student_Resource/SectionA/employee.cs
@@ -0,0 +1,29 @@
+using System;
+
+
+namespace SectionA{
+
+
+    public class Employee{
+
+        string NRIC {get;set;}
+
+        string Fullname {get;set;}
+
+        string Salutation {get;set;}
+
+        DateTime StartDate {get;set;}
+
+        string Designation {get;set;}
+
+        string Department {get;set;}
+
+        string MobileNo {get;set;}
+
+        string HireType {get;set;}
+
+        double salary {get;set;}
+
+        double MonthlyPayout  {get;set;}
+    }
+}
