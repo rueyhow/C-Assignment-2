947882870ce9fb4678b2a06e90ee2b1579b9bd2b
Your Name <sabertoothruey@gamil.com>
2022-07-07 03:17:29 +0800

    sqknbswq

--- ASN2_Student_Resource/SectionC/main.py
+++ ASN2_Student_Resource/SectionC/main.py
@@ -1,10 +1,19 @@
-print(sum(map(lambda employee: float(employee[-1]) * 0.85 if employee[7] == 'FullTime' and int(employee[3].split('/')[-1]) > 95 else 0,[employee.split('|') for employee in open("ASN2_Student_Resource\SectionB\HRMasterlistB.txt").read().split('\n')])))
+from functools import reduce
+print(
+    sum(
+        map(
+            lambda employee: float(employee[-1]) * 0.85 if employee[7] == 'FullTime' and int(employee[3].split('/')[-1]) > 95 else 0,
+                [employee.split('|') for employee in open("D:\Advanced Prog\Assignment 2\C-Assignment-2\ASN2_Student_Resource\SectionB\HRMasterlistB.txt").read().split('\n')])))
 
 
-
-
-
-
+print(
+    sum(
+        map(
+            lambda employee: float(employee[-1]) * 0.85 if employee[7] == 'FullTime' and int(employee[3].split('/')[-1]) > 95 else 0,
+                list(map(lambda employee : employee.split('|')  , open("D:\Advanced Prog\Assignment 2\C-Assignment-2\ASN2_Student_Resource\SectionB\HRMasterlistB.txt").read().split('\n')))
+        )
+    )
+)
 
 
 
