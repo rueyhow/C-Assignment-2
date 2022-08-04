1c6319d5d4642324f9fe92c64666af097a827001
Your Name <sabertoothruey@gamil.com>
2022-08-04 23:55:49 +0800

    dsds

--- ASN2_Student_Resource/SectionA/Program.cs
+++ ASN2_Student_Resource/SectionA/Program.cs
@@ -12,16 +12,14 @@ namespace SectionA
 
         public static List<Employee> readHRMasterList()
         {
-            List<string> allLinesText = System.IO.File.ReadAllLines(@"D:\sch stuff\y2\AVP\assignment2\C-Assignment-2\ASN2_Student_Resource\HRMasterlist.txt").ToList();
+            List<string> allLinesText = System.IO.File.ReadAllLines(@"D:\Advanced Prog\Assignment 2\C-Assignment-2\ASN2_Student_Resource\HRMasterlist.txt").ToList();
 
             List<Employee> listOfData = new List<Employee>(){};
             foreach(string employee in allLinesText)
             {
             
             string[] data = employee.Split("|");
-
-
-            listOfData.Add(new SectionA.Employee(data[0] ,data[1] , data[2] , Convert.ToDateTime(data[3]).Date , data[4] , data[5] , data[6] , data[7] , Convert.ToDouble(data[8])));
+            listOfData.Add(new SectionA.Employee(data[0] ,data[1] , data[2] , DateTime.ParseExact(data[3], "dd/MM/yyyy", null) , data[4] , data[5] , data[6] , data[7] , Convert.ToDouble(data[8])));
         };
 
         return listOfData;
@@ -29,6 +27,7 @@ namespace SectionA
 
         static void Main(string[] args)
         {
+            // readHRMasterList();
             DelegateAll del1 = info.generateInfoForCorpAdmin;
             DelegateAll del2 = info.generateInfoForITDepartment;
             DelegateAll del3 = info.generateInfoForProcurement;
